200ecacca9ba7fc66435b2247bf6053007bc0a78
Satoshi <satoshi@pop-os.localdomain>
2020-01-25 19:15:06 -0500

    correct clean_csv file

--- clean_school_csv.py
+++ clean_school_csv.py
@@ -18,27 +18,29 @@ df = df[df.male_graduates != null_major]
 
 # Replace null values with 0
 gender = ['male', 'female']
+categories_major = ['_high_school', '_employed']
 categories_minor = ['_part_time_high_school', '_home_and_high_school',
                     '_home', '_others']
+major_or_minor = ['_major', '_minor']
 
 for item_g in gender:
-    for item_a in categories_minor:
-        column = item_g+item_a
-        df.loc[:, column][df.loc[:, column] == null_minor] = 0
+    for item_m in major_or_minor:
+        for item_c in vars()['categories' + item_m]:
+            column = item_g + item_c
+            df.loc[:, column][df.loc[:, column] == vars()['null' + item_m]] = 0
 
 # Compare subtotal of categories and graduates
-categories_major = ['_high_school', '_employed']
 categories_all = categories_major + categories_minor
 
 for item_g in gender:
     df[item_g+'_subtotal'] = df.loc[:, item_g + categories_all[0]:
-                                    item_g+categories_all[-1]].sum(axis=1)
+                                    item_g + categories_all[-1]].sum(axis=1)
     # Get indices such that the subtotal is not equal to the # of graduates
     # May delete rows with these indecies
     vars()[item_g+'_sub_neq_grad_id'] = df[df[item_g + '_subtotal']
                                            != df[item_g + '_graduates']].index
     # Delete subtotal columns
-    del df[item_g+'_subtotal']
+    del df[item_g + '_subtotal']
 
 # Create cleaned dataset
 new_file_name = 'school_cleaned.csv'
