3d07e419f8e8419737fd717e9a100ab883c1ab15
Satoshi <satoshi@pop-os.localdomain>
2020-01-27 13:57:09 -0500

    group schools

---
+++ group_schools_temp.py
@@ -0,0 +1,24 @@
+# Classify schools
+
+import pandas as pd
+import numpy as np
+import os
+
+directory = '../../Data/School/0001'
+file_name = 'school.csv'
+file_read = os.path.join(directory, file_name)
+
+df = pd.read_csv(file_read)
+
+# Hypothesis: the first two digits in the school_id represents geography
+school_group = [item[:-1] for item in df.school_id.unique().astype(str)]
+school_group = np.unique(np.array(school_group).astype(int))
+
+# Add school groups to the dataframe
+for item in school_group:
+    search = item.astype(str)+'[0-9]$'
+    bool_match = df.school_id.astype(str).str.match(search)
+    df.loc[bool_match, 'school_group'] = item
+
+# Aggregate schools by groups
+df_school_grouped = df.iloc[:, 1:].groupby('school_group').sum()
