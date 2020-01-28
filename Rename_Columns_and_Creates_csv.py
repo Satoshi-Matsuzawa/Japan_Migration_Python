35a037c60dd35e4ea1d3bd62cf4e6803f96d4d43
Satoshi <satoshi@pop-os.localdomain>
2020-01-27 19:48:35 -0500

    cleaned codes

--- Rename_Columns_and_Creates_csv.py
+++ Rename_Columns_and_Creates_csv.py
@@ -1,64 +1,57 @@
+# Load dats file, rename columns, and create csv files
+
 import pandas as pd
 import os
 
 directory = '../../Data/School/0001'
+original_file_names = ['0001_kou', '0001_otsu', '0001_hei', '0001_tei']
+df = []
 
+for file_name in original_file_names:
+    original_file = os.path.join(directory, file_name+'.dat')
+    df.append(pd.read_csv(original_file, sep='\t', error_bad_lines=False))
 
-# Dataset for schools
-school_file = os.path.join(directory, '0001_kou.dat')
+# Column Names
+school_id = ['school_id']
 
-df_school = pd.read_csv(school_file, sep='\t', error_bad_lines=False)
-df_school.columns = ['school_id',
-                     'male_graduates', 'male_high_school',
-                     'male_part_time_high_school', 'male_employed',
-                     'male_home_and_high_school', 'male_home', 'male_others',
-                     'female_graduates', 'female_high_school',
-                     'female_part_time_high_school', 'female_employed',
-                     'female_home_and_high_school', 'female_home',
-                     'female_others']
+# For schools
+categories = ['_graduates', '_high_school', '_part_time_high_school',
+              '_employed', '_home_and_high_school', '_home', '_others']
+gender = ['male', 'female']
+gender_and_categories = []
+for g in gender:
+    for c in categories:
+        gender_and_categories.append(g + c)
 
-school_file_out = os.path.join(directory, 'school_statistics.csv')
-df_school.to_csv(school_file_out)
+# For students
+student_gender = ['gender']
+courses = ['course_1', 'course_2', 'course_3',
+           'course_4', 'course_5', 'course_6']
+guardian = ['heir', 'relation_to_the_guardian', 'occupation_of_the_guardian']
+family = [' living_standard', 'family_size']
+job = ['job_industry', 'job_location', 'job_firm_size',
+       'job_description', 'job_trainee_or_apprentice',
+       'job_starting_salary', 'job_commute_or_live_alone',
+       'job_employment_channel', 'attend_school']
+home = ['reasons_not_employed', 'career_plan', 'attending_school']
+high_school = ['academic_scores', 'specialization', 'career_plan']
 
-# Dataset for students who got employed
-employed_file = os.path.join(directory, '0001_otsu.dat')
+# Rename dataset for schools
+df[0].columns = school_id + gender_and_categories
 
-df_employed = pd.read_csv(employed_file, sep='\t', error_bad_lines=True)
-df_employed.columns = ['school_id', 'gender',
-                       'course_1', 'course_2', 'course_3',
-                       'course_4', 'course_5', 'course_6',
-                       'heir', 'relation_to_the_guardian',
-                       'occupation_of_the_guardian',
-                       'living_standard', 'family_size',
-                       'job_industry', 'job_location', 'job_firm_size',
-                       'job_description', 'job_trainee_or_apprentice',
-                       'job_starting_salary', 'job_commute_or_independent',
-                       'job_employment_channel', 'attend_school']
+# Rename dataset for students who got employed
+df[1].columns = school_id + student_gender + courses + guardian + family + job
 
-employed_file_out = os.path.join(directory, 'employed.csv')
-df_employed.to_csv(employed_file_out)
+# Rename dataset for students who stays at home
+df[2].columns = school_id + student_gender + courses + guardian + family + home
 
-# Dataset for students who stays at home
-home_file = os.path.join(directory, '0001_hei.dat')
+# Rename dataset for students who proceeded to higher education
+df[3].columns = school_id + student_gender + guardian + high_school
 
-df_home = pd.read_csv(home_file, sep='\t', error_bad_lines=True)
-df_home.columns = ['school_id', 'gender',
-                   'course_1', 'course_2', 'course_3',
-                   'course_4', 'course_5', 'course_6', 'heir',
-                   'relation_to_the_guardian', 'occupation_of_the_guardian',
-                   'living_standard', 'family_size',
-                   'reasons_not_employed', 'career_plan', 'attending_school']
+# Create CSV files
+new_file_names = ['school', 'student_employed',
+                  'student_home', 'student_high_school']
 
-home_file_out = os.path.join(directory, 'home.csv')
-df_home.to_csv(home_file_out)
-
-# Dataset for students who proceeded to higher education
-higher_ed_file = os.path.join(directory, '0001_tei.dat')
-
-df_higher_ed = pd.read_csv(higher_ed_file, sep='\t', error_bad_lines=True)
-df_higher_ed.columns = ['school_id', 'gender', 'heir', 'relation_to_guardian',
-                        'occupation_of_the_guardian', 'academic_scores',
-                        'specialization', 'career_plan']
-
-higher_ed_file_out = os.path.join(directory, 'higher_ed.csv')
-df_higher_ed.to_csv(higher_ed_file_out)
+for i in range(len(new_file_names)):
+    file_out = os.path.join(directory, new_file_names[i]+'.csv')
+    df[i].to_csv(file_out, index=False)
